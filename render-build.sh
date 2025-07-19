28b9ed45df3dd8c37595d904f5c0e5def91e685d
Jelle701 <Jelle701@gmail.com>
2025-07-19 14:51:53 +0200

    Remove unnecessary Render build script

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Stel de JAVA_HOME in (Render installeert Java 17 hier)
-export JAVA_HOME=/opt/java/openjdk-17
-
-# Voer het build commando uit
-./mvnw clean install -DskipTests
